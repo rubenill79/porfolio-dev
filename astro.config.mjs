00d2d0bdd4b2f099e640d4dbc6a61e300aa3ef42
Rubén Lázaro <rubenill79@hotmail.com>
2024-09-22 21:27:26 +0200

    adaptador ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+
   i18n: {
     defaultLocale: "en",
     locales: ["es", "en"],
     routing: {
       prefixDefaultLocale: true
    }
-  }
+  },
+
+  adapter: vercel()
 });
