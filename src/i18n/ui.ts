271fe68fb6f77294f13ea99356d120602e95fce8
Rubén Lázaro <rubenill79@hotmail.com>
2024-09-24 18:52:03 +0200

    meta y internacionalización

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.projects': 'Projects',
+    },
+    es: {
+      'nav.home': 'Home',
+      'nav.about': 'Sobre Mí',
+      'nav.projects': 'Proyectos',
+    },
+  } as const;
