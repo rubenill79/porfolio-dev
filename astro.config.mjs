55da7632befafb9dfb58d25ddedd71aa4a0d6bd7
Rubén Lázaro <rubenill79@hotmail.com>
2024-09-29 14:42:05 +0200

    more metadata and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,23 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  site: 'https://rubenlazaro-dev.vercel.app',
+  integrations: [tailwind(), 
+    sitemap({
+      i18n: {
+        defaultLocale: "",
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          es: 'es-ES'
+        }
+      }
+    },
+  )],
 
   i18n: {
     defaultLocale: "en",
